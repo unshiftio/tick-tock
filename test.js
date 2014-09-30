0de7bec88aa742c0d37608b032afb1fb7cfd1225
Arnout Kazemier <info@3rd-Eden.com>
2014-09-30 11:00:27 +0200

    [test] Adding tests against the methods.

---
+++ test.js
@@ -0,0 +1,162 @@
+describe('ticktock', function () {
+  'use strict';
+
+  var assume = require('assume')
+    , Tick = require('./')
+    , tock;
+
+  beforeEach(function () {
+    tock = new Tick();
+  });
+
+  afterEach(function () {
+    tock.clear();
+  });
+
+  this.timeout(30000);
+
+  it('is exported as a function', function () {
+    assume(Tick).is.a('function');
+  });
+
+  describe('.parse', function () {
+    it('should preserve ms', function () {
+      assume(Tick.parse('100')).to.equal(100);
+    });
+
+    it('should convert from m to ms', function () {
+      assume(Tick.parse('1m')).to.equal(60000);
+    });
+
+    it('should convert from h to ms', function () {
+      assume(Tick.parse('1h')).to.equal(3600000);
+    });
+
+    it('should convert d to ms', function () {
+      assume(Tick.parse('2d')).to.equal(172800000);
+    });
+
+    it('should convert s to ms', function () {
+      assume(Tick.parse('1s')).to.equal(1000);
+    });
+
+    it('should convert ms to ms', function () {
+      assume(Tick.parse('100ms')).to.equal(100);
+    });
+
+    it('should work with decimals', function () {
+      assume(Tick.parse('1.5h')).to.equal(5400000);
+    });
+
+    it('should work with multiple spaces', function () {
+      assume(Tick.parse('1   s')).to.equal(1000);
+    });
+
+    it('should return 0 if invalid', function () {
+      assume(Tick.parse('Hello mom')).to.equal(0);
+    });
+
+    it('should be case-insensitive', function () {
+      assume(Tick.parse('1.5H')).to.equal(5400000);
+    });
+
+    it('should work with numbers starting with .', function () {
+      assume(Tick.parse('.5ms')).to.equal(0.5);
+    });
+  });
+
+  describe('#setTimeout', function () {
+    it('adds a setTimeout', function (next) {
+      var start = Date.now();
+
+      tock.setTimeout('test', function () {
+        var taken = Date.now() - start;
+
+        assume(taken).is.above(5);
+        assume(taken).is.below(15);
+
+        next();
+      }, 10);
+    });
+
+    it('accepts strings for time', function (next) {
+      var start = Date.now();
+
+      tock.setTimeout('test', function () {
+        var taken = Date.now() - start;
+
+        assume(taken).is.above(5);
+        assume(taken).is.below(15);
+
+        next();
+      }, '10 ms');
+    });
+
+    it('run with the same timeout if a known name is provided', function (next) {
+      var start = Date.now();
+
+      tock.setTimeout('test', function () {
+        var taken = Date.now() - start;
+
+        assume(taken).is.above(95);
+        assume(taken).is.below(110);
+      }, '100 ms');
+
+      setTimeout(function () {
+        tock.setTimeout('test', function () {
+          var taken = Date.now() - start;
+
+          assume(taken).is.above(95);
+          assume(taken).is.below(110);
+
+          next();
+        }, '100 ms');
+      }, 20);
+    });
+  });
+
+  describe('#clear', function () {
+    function fail() {
+      throw new Error('I should never be executed');
+    }
+
+    it('clears multiple timeouts', function (next) {
+      tock.setTimeout('timer', fail, '1 second');
+      tock.setTimeout('timer', fail, '1 second');
+      tock.setTimeout('timers', fail, '10 ms');
+      tock.setTimeout('timers', fail, 0);
+
+      tock.clear('timer', 'timers');
+
+      setTimeout(function () {
+        next();
+      }, 1010);
+    });
+
+    it('splits the string if only one argument is supplied', function (next) {
+      tock.setTimeout('timer', fail, '1 second');
+      tock.setTimeout('timer', fail, '1 second');
+      tock.setTimeout('timers', fail, '10 ms');
+      tock.setTimeout('timers', fail, 0);
+
+      tock.clear('timer, timers, non-existing');
+
+      setTimeout(function () {
+        next();
+      }, 1010);
+    });
+
+    it('clears all if no arguments are supplied', function (next) {
+      tock.setTimeout('timer', fail, '1 second');
+      tock.setTimeout('timer', fail, '1 second');
+      tock.setTimeout('timers', fail, '10 ms');
+      tock.setTimeout('timers', fail, 0);
+
+      tock.clear();
+
+      setTimeout(function () {
+        next();
+      }, 1010);
+    });
+  });
+});
