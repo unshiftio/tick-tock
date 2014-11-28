085791af68d01733c32d22346b40dbe58e910711
Arnout Kazemier <info@3rd-Eden.com>
2014-11-28 10:43:43 +0100

    [dist] 0.0.9
    The parser has moved to it's own module called `millisecond`

--- index.js
+++ index.js
@@ -217,52 +217,7 @@ Tick.prototype.adjust = function adjust(name, ms) {
  * @returns {Number}
  * @api private
  */
-Tick.parse = function time(ms) {
-  if ('string' !== typeof ms || +ms) return +ms;
-
-  var match = /^((?:\d+)?\.?\d+) *(milliseconds?|msecs?|ms|seconds?|secs?|s|minutes?|mins?|m|hours?|hrs?|h|days?|d|years?|yrs?|y)?$/i.exec(ms)
-    , second = 1000
-    , minute = second * 60
-    , hour = minute * 60
-    , day = hour * 24
-    , amount;
-
-  if (!match) return 0;
-
-  amount = parseFloat(match[1]);
-
-  switch (match[2].toLowerCase()) {
-    case 'days':
-    case 'day':
-    case 'd':
-      return amount * day;
-
-    case 'hours':
-    case 'hour':
-    case 'hrs':
-    case 'hr':
-    case 'h':
-      return amount * hour;
-
-    case 'minutes':
-    case 'minute':
-    case 'mins':
-    case 'min':
-    case 'm':
-      return amount * minute;
-
-    case 'seconds':
-    case 'second':
-    case 'secs':
-    case 'sec':
-    case 's':
-      return amount * second;
-
-    default:
-      return amount;
-  }
-};
-
+Tick.parse = require('millisecond');
 //
 // Expose the timer factory.
 //
