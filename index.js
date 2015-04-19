unshiftio/tick-tock

commit e1dba13bd439efdf1c8013ea71fb8c07a041543d

wrap all clear functions to fix binding issue in Chrome

index.js
@@ -64,7 +64,7 @@ Tick.prototype.setTimeout = function timeout(name, fn, time) {
 
   tick.timers[name] = {
     timer: setTimeout(tick.tock(name, true), ms(time)),
-    clear: clearTimeout,
+    clear: function(id) { clearTimeout(id); },
     fns: [fn]
   };
 
@@ -90,7 +90,7 @@ Tick.prototype.setInterval = function interval(name, fn, time) {
 
   tick.timers[name] = {
     timer: setInterval(tick.tock(name), ms(time)),
-    clear: clearInterval.bind(null),
+    clear: function(id) { clearInterval(id); },
     fns: [fn]
   };
 
@@ -117,7 +117,7 @@ Tick.prototype.setImmediate = function immediate(name, fn) {
 
   tick.timers[name] = {
     timer: setImmediate(tick.tock(name, true)),
-    clear: clearImmediate,
+    clear: function(id) { clearImmediate(id); },
     fns: [fn]
   };
 

commit a9b38bae599e0522cf588e2e213a2f1234b41d03

[fix] Correctly adjust the delay of the timer
Fixes a bug introduced in commit e1dba13b where `setInterval` was
replaced with `setTimeout` when adjusting the delay of the timer.

index.js
@@ -64,7 +64,7 @@ Tick.prototype.setTimeout = function timeout(name, fn, time) {
 
   tick.timers[name] = {
     timer: setTimeout(tick.tock(name, true), ms(time)),
-    clear: function clear(id) { clearTimeout(id); },
+    clear: function unsetTimeout(id) { clearTimeout(id); },
     fns: [fn]
   };
 
@@ -90,7 +90,7 @@ Tick.prototype.setInterval = function interval(name, fn, time) {
 
   tick.timers[name] = {
     timer: setInterval(tick.tock(name), ms(time)),
-    clear: function clear(id) { clearInterval(id); },
+    clear: function unsetInterval(id) { clearInterval(id); },
     fns: [fn]
   };
 
@@ -117,7 +117,7 @@ Tick.prototype.setImmediate = function immediate(name, fn) {
 
   tick.timers[name] = {
     timer: setImmediate(tick.tock(name, true)),
-    clear: function clear(id) { clearImmediate(id); },
+    clear: function unsetImmediate(id) { clearImmediate(id); },
     fns: [fn]
   };
 
@@ -199,7 +199,7 @@ Tick.prototype.adjust = function adjust(name, time) {
 
   if (!timer) return tick;
 
-  interval = timer.clear === clearInterval;
+  interval = timer.clear.name === 'unsetInterval';
   timer.clear(timer.timer);
   timer.timer = (interval ? setInterval : setTimeout)(tick.tock(name, !interval), ms(time));
 

test.js
@@ -329,24 +329,22 @@ describe('ticktock', function () {
     });
 
     it('adjusts the interval', function (next) {
-      var ticked = false;
+      var start = Date.now()
+        , counter = 0
+        , elapsed;
 
       tock.setInterval('foo', function () {
-        var spend = Date.now() - start;
+        elapsed = Date.now() - start;
 
-        if (spend < 30) {
+        if (++counter === 1) {
+          assume(elapsed).is.below(30);
           tock.adjust('foo', '100 ms');
-          ticked = true;
-        } else if(spend < 150) {
-          if (!ticked) throw new Error('I should have ticked');
+        } else if (counter === 3) {
+          assume(elapsed).is.between(200, 250);
           tock.clear();
           next();
         }
-
-        start = Date.now();
       });
-
-      var start = Date.now();
     });
   });
 